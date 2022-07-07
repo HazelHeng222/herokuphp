fc8148ec587c76ec6088bd6cbb66afb08a16fa67
HazelHeng222 <hhazel.heng@gmail.com>
2022-07-07 15:13:14 +0800

    updates

--- web/test.php
+++ web/test.php
@@ -5,8 +5,9 @@
   <!-- fix: Document does not have <title> element -->
   <title>Test Page</title>
   <!-- fix: Document does not have a <meta name="viewport"> tag with width or initial-scale -->
-  <!-- fix: Document does not have a meta description -->
-  <meta name="viewport" content="width=device-width, initial-scale=1" content="Test page to explore and experiment Google Lighthouse and Heroku">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!-- fix: Document does not have a meta description -->
+  <meta name="description" content="Test page to explore and experiment Google Lighthouse and Heroku">
 </head>
 <body>
   Hello World!
