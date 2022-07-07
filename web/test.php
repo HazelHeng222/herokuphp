d504384750e5527e2032cc99e580078d0bf16697
HazelHeng222 <hhazel.heng@gmail.com>
2022-07-07 14:59:25 +0800

    updates

--- web/test.php
+++ web/test.php
@@ -1,5 +1,8 @@
+<!-- fix: Page lacks the HTML doctype -->
+<!DOCTYPE html>
 <html lang="en">
 <head>
+  <!-- fix: Document does not have <title> element -->
   <title>Test Page</title>
 </head>
 <body>
