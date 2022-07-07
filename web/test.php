a5ed66a54615178863ad21dcfff377663e69921f
HazelHeng222 <hhazel.heng@gmail.com>
2022-07-07 15:00:44 +0800

    updates

--- web/test.php
+++ web/test.php
@@ -4,6 +4,8 @@
 <head>
   <!-- fix: Document does not have <title> element -->
   <title>Test Page</title>
+  <!-- fix: Document does not have a <meta name="viewport"> tag with width or initial-scale -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
   Hello World!
